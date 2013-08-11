cc78e0154e097d4db82f5bbf0aaad6b1a6a85fec
Ruan van Zyl <vanzyl222@yahoo.com>
2013-08-11 12:52:02 +0200

    Update Email.py

--- Email.py
+++ Email.py
@@ -6,25 +6,32 @@ import time
 
 from email.parser import HeaderParser
 
-obj = imaplib.IMAP4_SSL('imap.mail.yahoo.com','993')																#Gmail IMAP server address: imap.gmail.com, leave port 993.
+obj = imaplib.IMAP4_SSL('imap.mail.yahoo.com','993')        															#Gmail IMAP server address: imap.gmail.com, leave port 993.
 username = "USERNAME"																								#Enter your own username, everything before the @ sign.
 password = "PASSWORD"																								#Your email password.
 obj.login(username, password)
-obj.select()
-emailamount = str(len(obj.search(None, 'UnSeen')[1][0].split()))
-emailamount = int(emailamount)
+first_loop = True
+update = False
+buf_new_mails = 0
 
 while True:
-
-        emailamount = str(len(obj.search(None, 'UnSeen')[1][0].split()))
-        obj.select()
         
-        print ("You have " + str(emailamount) + "\nnew emails.")
+        obj.select()
+        emailamount = str(len(obj.search(None, 'UnSeen')[1][0].split()))
+        
+        emailamount = int(emailamount)
+
+        if emailamount != buf_new_mails or first_loop == True:													
+                buf_new_mails = emailamount
+                first_loop = False
+                update = True
+        
         
         time.sleep(1)
                 
-        if emailamount >= 1:
-                        
+        if update == True:																							#Only update when the number of unread emails change.
+                
+                        print ("You have " + str(emailamount) + " new emails.\n")
                         for n in range(0, int(emailamount)):
                         
                                 data = obj.search(None, 'ALL')
@@ -39,10 +46,11 @@ while True:
                                 subject = msg[1]
                                 from_who = msg[2]
                                 from_who = from_who.split()
+                                print str(n+1) + ": "
                                 print (from_who[0] + " " + from_who[1] + "\n")
                                 time.sleep(2)
-                                print(subject)
+                                print(subject + "\n")
                                 time.sleep(2)
-
+        update = False
 
         #time.sleep(1)
